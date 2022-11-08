ba114c6b62ae4d166b3a76992f7129570c6e7d6e
Antoine <dhainaut.antoine98@gmail.com>
2022-11-08 14:23:52 +0100

    somes add

---
+++ src/main/resources/db/migration/list.sql
@@ -0,0 +1,3 @@
+insert into animal(name , category , price ) values ('chat' , 'félin' , 20.00);
+insert into animal(name , category , price ) values ('chien' , 'canidé' , 18.00);
+insert into animal(name , category , price ) values ('baleine' , 'aquatique' , 200.00);
